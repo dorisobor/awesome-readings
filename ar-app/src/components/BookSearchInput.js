7c07734fb1037bd67f4dc1c09f816d0ab9a6b8e4
dorisobor <troyamber@live.com>
2018-12-10 22:39:06 +0100

    testing api

--- ar-app/src/components/BookSearchInput.js
+++ ar-app/src/components/BookSearchInput.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import {Form, FormGroup, Label, Input } from "reactstrap";
+import Request from 'superagent-es6-promise';
 
+
+
+Request.get('https://www.googleapis.com/books/v1/volumes?q=harry%20potter')
+.then(response => console.log(response.body))
+.catch(error =>  console.log(error))
 
 
   class BookSearchInput extends Component  {
