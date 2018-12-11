9e6b307b7cb7300e58c00b13c2fde5b1af7ae773
dorisobor <troyamber@live.com>
2018-12-11 04:33:32 +0100

    added an api to fetch

--- ar-app/src/components/BookSearchInput.js
+++ ar-app/src/components/BookSearchInput.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
 import {Form, FormGroup, Label, Input } from "reactstrap";
-import Request from 'superagent-es6-promise';
+// import _ from 'lodash';
+// import axios from 'axios';
+import SearchGallery from './BookSearchStyling.js';
+
+class BookSearchInput extends Component {
+  // set state
+  constructor(props) {
+    super(props);
+
+  this.state= {
+    bookTitleSearch:'',
+    items : []
+  };
+}
+  // get the value from the input
+  handleChange = event => {
+    this.setState({ bookTitleSearch: event.target.value });
+  };
 
 
+  handleSubmit= event => {
+    event.preventDefault();
+     const url = `https://www.googleapis.com/books/v1/volumes?q=title:${this.state.bookTitleSearch}`;
+    fetch(url, {method:"GET"})
+    .then(response =>  response.json())
+    .then(json => {
+      let {items} = json;
+      this.setState({
+        items : items
+      })
 
-Request.get('https://www.googleapis.com/books/v1/volumes?q=harry%20potter')
-.then(response => console.log(response.body))
-.catch(error =>  console.log(error))
+    })
+
+    // .catch(e => console.log('error', e));
+    console.log("clicked on search  button" , this.state.bookTitleSearch);
+
+}
+
+render() {
 
 
-  class BookSearchInput extends Component  {
-  render() {
     return (
-      <Form>
+      <div>
+      <Form onSubmit={this.handleSubmit}>
         <FormGroup>
           <Label for="exampleSearch"> Search </Label>{" "}
           <Input
             type="search"
-            name="search"
-            id="exampleSearch"
+            name="BookSearchInput"
+            id="BookSearchInput"
             placeholder="search placeholder"
+            value={this.state.BookSearchInput}
+            onChange={this.handleChange}
           />
         </FormGroup>
       </Form>
+      <SearchGallery items={this.state.items} />
+
+     </div>
     );
   }
 }
---
+++ ar-app/src/components/BookSearchStyling.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class SearchGallery extends Component{
+    render(){
+        return (
+            <div>
+            {
+                this.props.items.map((item , i) => {
+                    let {title, imageLinks , infoLink} = item.volumeInfo
+                    return (
+                        <a href ={infoLink}
+                        target = "_blank"
+                        key={i} className = "book">
+                        <img
+                        src ={imageLinks !== undefined? imageLinks.thumbnail : ''}
+                        alt = "book image"
+                        className = "bookImage"
+                        />
+                        <div className = "titleText">{title }</div>
+
+                        </a>
+
+                    );
+                })
+            }</div>
+
+        );
+    }
+}
+
+export default SearchGallery;
