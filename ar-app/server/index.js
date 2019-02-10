d64991a282e2ae1b3fab3c0bd0adcd303cbb7708
dorisobor <troyamber@live.com>
2019-02-10 22:26:14 +0100

    finished with mail function

--- ar-app/server/index.js
+++ ar-app/server/index.js
@@ -6,14 +6,41 @@ const cors = require('cors');
 const app = express(); //alias from the express function
 
 //sendgrid api key
-sgMail.setApiKey('____YOUR___API__KEY');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(cors()); //utilize Cors so the browser doesn't restrict data, without it Sendgrid will not send!
 
-// Express server Welcome page
+// Welcome page of the express server:
 app.get('/', (req, res) => {
-    res.send("Welcome to the Sendgrid Emailing Server");
+    res.send("Welcome to the email Server");
 });
+
+app.get('/send-email', (req, res) => {
+
+    const {recipient, sendersEmail, subject, text } = req.query;
+
+    //Sendgrid variables
+    const message = {
+        to: recipient,
+        from: 'example@mail.com',
+        subject: subject,
+        text: text,
+        name: 'example'
+    };
+
+    //Sends the Email
+    sgMail.send(message)
+        .then(() => {
+            //Celebrate
+            console.log(message);
+        })
+        .catch(error => {
+
+            //Log friendly error
+            console.error(error.toString());
+        });
+});
+
 
 
 // run 'nodemon index.js' to acess server on port 4000
